d502b507a05eaf14b0dbe90458c0a41801f34fa1
shenjiaming <shenjiaming@rd.netease.com>
2022-10-23 20:16:57 +1100

    training

--- atd12k.py
+++ atd12k.py
@@ -69,14 +69,14 @@ class ATD12k(Dataset):
 
             images = images[:3]
 
-            return images, 3
+            return images
         else:
             T = self.transforms
             images = [T(img_) for img_ in images]
 
             images = images[:3]
 
-            return images, 3
+            return images
 
     def __len__(self):
         if self.training:
